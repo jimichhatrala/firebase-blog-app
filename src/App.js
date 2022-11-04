2c7aaf182d34225f197248843525c62d1a9e1aee
jimichhatrala <jimichhatrala@gmail.com>
2022-11-04 20:13:05 +0530

    Create/Read/Update/Spinner added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "./style.scss";
 import "./media-query.css";
 import Home from "./pages/Home";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Detail from "./pages/Detail";
@@ -49,10 +49,24 @@ function App() {
       />
       <ToastContainer position="top-center" />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/create" element={<AddEditBlog />} />
-        <Route path="/update/:id" element={<AddEditBlog />} />
+        <Route path="/" element={<Home setActive={setActive} user={user} />} />
+        <Route path="/detail/:id" element={<Detail setActive={setActive} />} />
+        <Route
+          path="/create"
+          element={
+            user?.uid ? <AddEditBlog user={user} /> : <Navigate to="/" />
+          }
+        />
+        <Route
+          path="/update/:id"
+          element={
+            user?.uid ? (
+              <AddEditBlog user={user} setActive={setActive} />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
         <Route path="/about" element={<About />} />
         <Route path="/auth" element={<Auth setActive={setActive} />} />
         <Route path="*" element={<NotFound />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import { collection, deleteDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import BlogSection from "../components/BlogSection";
+import Spinner from "../components/Spinner";
 
-const Home = () => {
+import { db } from "../firebase";
+
+const Home = ({ setActive, user }) => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      collection(db, "blogs"),
+      (snapshot) => {
+        let list = [];
+        snapshot.docs.forEach((doc) => {
+          list.push({ id: doc.id, ...doc.data() });
+        });
+        setBlogs(list);
+        setLoading(false);
+        setActive("home");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    return () => {
+      unsub();
+    };
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  const handleDelete = async (id) => {
+    if (window.confirm("Are you sure to delete this blog ?")) {
+      try {
+        setLoading(true);
+        await deleteDoc(doc(db, "blogs", id));
+        toast.success("Blog deleted successfully.");
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
-    <h2>Home</h2>
-  )
-}
+    <div className="container-fluid pb-4 pt-4 padding">
+      <div className="container padding">
+        <div className="row mx-0">
+          <h2>Trending</h2>
+          <div className="col-md-8">
+            <BlogSection
+              blogs={blogs}
+              user={user}
+              handleDelete={handleDelete}
+            />
+          </div>
+          <div className="col-md-3">
+            <h2>Tags</h2>
+            <h2>Most Popular</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
